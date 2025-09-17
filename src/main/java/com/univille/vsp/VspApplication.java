22df11f629ba8a2e9c85fda57b28d3ccf5c812ff
Irineu119 <vinicrezende@gmail.com>
2025-09-17 20:05:23 -0300

    1

---
+++ src/main/java/com/univille/vsp/VspApplication.java
@@ -0,0 +1,13 @@
+package com.univille.vsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univille/vsp/VspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univille.vsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
